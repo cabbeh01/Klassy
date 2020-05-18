e0147953886519cebb2ebdb117e7d7dd88899f1b
Cabbeh01 <cabbe.h01@gmail.com>
2020-05-18 16:24:25 +0200

    Eftermiddag

--- router/routes.js
+++ router/routes.js
@@ -8,14 +8,14 @@ const valiInputCode = require("../validationCode.js");
 
 module.exports = async function(app,io){
     
-    //Main page
+    //Startsida
     app.get("/", auth, async function(req,res){
         
         try{
+            //Kontrollerar ifall användaren är inloggad
             if(app.currentGroup == "1" || app.currentGroup == "0"){
-                
+                //Hämtar användar datan från mongoDB databas
                 user = await getUser(req,res);
-
                 res.render('index',{title:"Home",layout:"loggedin", user:user});
             }
             else{
@@ -29,33 +29,45 @@ module.exports = async function(app,io){
         
     });
 
-    //Login
+    //GET- Login detta visas när användaren kommer till login
     app.get("/login",loggedinRedirector,function(req,res){
         res.render('login',{title:"Inlogging"});
     });
 
+    //När användaren skrivit in sina inloggningsuppgifter kommer de att berabetas
+    //Först valideras inmatningen
     app.post("/login",valiInputLogin, async function(req,res){
 
         try{
+            //Lägger email och lösen i separata varibler så det blir lätt att komma åt
             let inEmail = req.body.email;
             let pass = req.body.password;
             
+            //Om inget fel har uppstått
             if(!req.err){
+                
+                //Leta upp eposten på databasen
                 await app.users.findOne({"email": inEmail},function(err,data){
     
+                    //Hittar den en användare som har eposten
                     if(!(data == null)){
-                        //console.log(data.password);
-                        //console.log(pass);
+                       
+                        //Kollar ifall användaren är verifierad och är den det så fortsätter inloggningen
                         if(data.verified){
+
+                            //Lösenorden jämförs
                             bcrypt.compare(pass,data.password,function(err,succ){
         
-                                console.log(err);
-                                console.log(succ);
+                                //Fungerar allt som det ska och lösenorden stämmer överens
                                 if(succ){
-                                    
-                                    const token = jwt.sign(data,process.env.PRIVATEKEY,{expiresIn:3600});
+
+                                    //Skapar en token som ska lagras i cookien så att användaren kan gå mellan routes utan att behöva
+                                    //logga in igen. Detta har jag löst med att använda JWT (Json Web Token) detta bibliotek gör att
+                                    //man kan lagra information krypterat. Samt sätta en ålder på det
+                                    const token = jwt.sign(data,process.env.PRIVATEKEY,{expiresIn:"1 day"});
                                     res.cookie("token",token,{httpOnly:true, maxAge:(2 * 24 * 60 * 60 * 1000),sameSite: 'strict'});
             
+                                    //Beroende på vilken behörighetsgrupp man tillhör så redirectar man till olika saker
                                     switch(data.group){
                                         case "0":
                                             res.redirect("/teacher");
@@ -66,7 +78,8 @@ module.exports = async function(app,io){
                                     }
                                 }
                                 else{
-                                    res.render('login',{title:"Registrering", errmess:"<script>alertify.error('Det går inte att logga in');</script>", email:req.body.email});
+                                    res.render('login',{title:"Registrering",
+                                    errmess:"<script>alertify.error('Det går inte att logga in');</script>", email:req.body.email});
                                 }
                             });
                         }
@@ -94,37 +107,65 @@ module.exports = async function(app,io){
     });
 
 
-    //Register
+    //Registrering
+    //GET-route som läser in html för användaren med hjälp av ett bibliotek som heter handelbars
     app.get("/register",loggedinRedirector,function(req,res){
         res.render('register',{title:"Registrering"});
     });
 
+    //När användaren klickar på en submitknapp i ett formulär hamnar hen i en postfunktion
+    //När användaren registrerat sig kontrolleras först inmatningen i en middleware valiInputRegister
     app.post("/register",valiInputRegister,async function(req,res){
         try{
+
+            //Kontrollerar så att inte problem har uppstått
             if(!req.err){
+                //Jag användar mig av ett bibliotek som heter Bcrypt. Det gör jag för att säkerställa att
+                //användarens lösenord kan förvaras säkert på databasen. Varje lösenord går igenom en process
+                //där datorn lägger och slumpar runt teckena. Detta kommer göras 12 gånger. En hash kan inte knäckas
+                //utan kan bara jämföras med en annan hash för att se ifall de är korrekta.
                 bcrypt.hash(req.body.password,12,async function(err,hash){
+                    
+                    //Hämtar email
                     let inEmail = req.body.email;
                     
+                        //Tar reda på ifall emailen finns i databasen
                         await app.users.findOne({"email": inEmail},async function(err,data){
                         
+                            //Om den inte finns
                             if(data == null){
-                                
+                                //Sätter vi hashen till användarens lösenord
                                 req.body.password = hash;
+
+                                //Tar bort repassword i objektet som är i klartext
                                 delete req.body.repassword;
+
+                                //Sätter användaren till ej verifierad då jag använder mig av tvåstegsverifiering
                                 req.body.verified =  false;
+
+                                //Genererar en verifieringskod
                                 let code = codeGen(26);
+
+                                //Lägger in verifieringskod i objektet
                                 req.body.verifyCode = code;
+
+                                //Lägger till användaren i användardatabasen
                                 await app.users.insertOne(req.body,function(err,result){
                                     
-                                    //console.log(err);
-                                    //console.log(result.insertedId);
+                                    //Skickar ett bekräftelsemail mail till användarens-mail.
                                     mail(req.body.email,"Verify account","Var vänligen och verifiera dig!","http://localhost:2380/confirm/"+result.ops[0]._id+"/"+code);
-                                    
                                 });
-                                res.render('login',{title:"Registrering avklarad", errmess:"<script>alertify.warning('Du är nu registrerad, ett mail har skickats till din inkorg. Vänligen bekräfta ditt konto!');</script>", name: req.body.name, class: req.body.class, email: req.body.email});
+                                
+                                //Skickar en html sida till användaren att registrering gick igenom och berättar för hen att hen ska verifiera sitt konto med sin mail
+                                res.render('login',{title:"Registrering avklarad", 
+                                errmess:"<script>alertify.warning('Du är nu registrerad, ett mail har skickats till din inkorg. Vänligen bekräfta ditt konto!');</script>", 
+                                name: req.body.name, class: req.body.class, email: req.body.email});
                             }
                             else{
-                                res.render('register',{title:"Registrering", errmess:"<script>alertify.error('Det går inte att registrera');</script>", name: req.body.name, class: req.body.class, email: req.body.email});
+                                
+                                //Det gick inte registrera html, fast behåller de inmatade värdena förutom lösenordet
+                                res.render('register',{title:"Registrering", errmess:"<script>alertify.error('Det går inte att registrera');</script>", name: req.body.name, 
+                                class: req.body.class, email: req.body.email});
                             }
                             
                         });
@@ -132,31 +173,48 @@ module.exports = async function(app,io){
     
             }
             else{
-                //console.log(req.err);
-                res.render('register',{title:"Registrering", errmess:"<script>alertify.error('Det går inte att registrera');</script>", name: req.body.name, class: req.body.class, email: req.body.email});
+                //Felmeddelande skickas
+                res.render('register',{title:"Registrering", errmess:"<script>alertify.error('Det går inte att registrera');</script>", name: req.body.name, 
+                class: req.body.class, email: req.body.email});
             }
         }
         catch{
+            //Skulle det inte alls fungera så startas routen om
             res.redirect("/register");
         }
-        //res.send(req.body.group);
     });
 
-    //Confirmation after registation
+    //Tvåstegsverifiering efter att man har registrerat sig
     app.get("/confirm/:id/:code", async function(req,res){
         try{
+            //När anändaren registrerar sig skickas en länk i detta formatet 
+            //Exempel  http://localhost:2380/confirm/hdjksahdsuai/djfldsfhsduflidshfsdflsd
+            //                                          (id)                (code)
+            
+            //Hämtar id:et som följde med i epost-meddelandet
             let id = req.params.id;
+            
+            //Letar upp användaren med id:et
             await app.users.findOne({"_id": app.objID(id)},async function(err,data){
-                //console.log(data);
+                
                 try{
+                    //Om den inte är verifierad
                     if(!data.verified){
+                        
+                        //Kontrollerar så att verifieringskoderna stämmer överens
                         if(data.verifyCode == req.params.code){
+                            //Ändrar så att användaren är veriferad
                             data.verified = true;
+
+                            //Uppdaterar användaren på databasen
                             await app.users.updateOne({"_id":app.objID(id)},{$set:{verified:true}},function(err){
                                 console.log(err);
                             });
+
+                            //Meddelar användaren att hen är veriferad
                             res.render("index",{title:"Verifierad",mess:"<script>alertify.success('Det är nu verifierad!');</script>"});
                         }
+                        //Sedan massa felmeddelande ifall det inte fungerade
                         else{
                             res.render("index",{title:"Det gick inte att verifiera",mess:"<script>alertify.error('Det gick inte att verifiera');</script>"});
                         }
@@ -501,15 +559,20 @@ module.exports = async function(app,io){
 
     async function teacherOnly(req,res,next){
         try{
+            //Hämtar användaren
             user = await getUser(req,res);
+            //Kollar ifall användaren är en lärare
             if(user.group == 0){
+                //Är användaren det så fortsätter vi in i routen
                 next();
             }
             else{
+                //Annars redirectar vi till elevens sida
                 res.redirect("/pupil");
             }
         }
         catch{
+            //Fungerar inget så redirectar vi till startsidan
             res.redirect("/");
         }
     }
@@ -577,12 +640,17 @@ module.exports = async function(app,io){
 
     function loggedinRedirector(req,res,next){
         try{
+            //Kollar ifall det är en lärare inloggad och skickar
+            //Läraren till sin sida
             if(app.currentGroup == "0"){
                 res.redirect("/teacher");
             }
+            //Kollar ifall det är en elev inloggad och skickar
+            //eleven till sin sida
             else if(app.currentGroup == "1"){
                 res.redirect("/pupil");
             }
+            //Annars skickas användaren vidare till routen
             else{
                 next();
             }
--- validationRegister.js
+++ validationRegister.js
@@ -1,23 +1,47 @@
+
 //Här sker validering av inmatningen på registeringen i Klassy
+//Importerar ett bibliotek som heter Joi. Detta används för att validera inmatningen på servern
+//Jag har även använt mig av validering på client sidan men det kan man som användare passera ifall
+//Man har kunskaper om html osv.
 
 const Joi = require("@hapi/joi");
 
+//Jag skapar en modul som jag exporterar
+//Valideringen kan ta tid därför jag använt mig av async
 module.exports = async function(req,res,next){
+
+    //Först skapar jag ett så kallat schema som är en mall på de objekt som ska valideras
+    //Samt vad för typ av validering som ska ske på de olika variablarna
     const schema = Joi.object({
+        //T.ex. namnet ska vara en sträng med max 50 tecken
         name: Joi.string().max(50),
+        //Emailen ska vara en sträng och en mail. Den är ett måste också att man skriver in
         email: Joi.string().email().required(),
+        //Lösenordet är en sträng med minst 7 tecken och max 20 tecken
         password: Joi.string().min(7).max(20),
+        //Repeatpassword ska stämma överens med password
         repeat_password: Joi.ref('password')
     });
 
     
     try {
-        const value = await schema.validateAsync({ name:req.body.name, email: req.body.email, password: req.body.password, repeat_password:req.body.repassword});
+        //Här kopplar jag ihop objektet jag får från post formen så att respektive värde kan valideras
+        //Detta görs föra att undvika XSS scripting
+        await schema.validateAsync({ 
+            name:req.body.name, 
+            email: req.body.email, 
+            password: req.body.password, 
+            repeat_password:req.body.repassword});
+        
         schema.validate(req.body);
+        
+        //Går allt som det ska skickas användaren vidare till den nästkommande middlewaren eller in i routen
         next();
     }
     catch (err) { 
+        //Uppstår ett fel lagras det i req.err och tas upp i routen
         req.err = err;
         next();
     }
 }
+
